e77a01af1657d789343181f5ca7f770887fe9823
Kole Nunley <Kfnunley@gmail.com>
2021-11-15 14:35:20 +0000

    convolution work

--- packages/math/src/lib.rs
+++ packages/math/src/lib.rs
@@ -1,8 +1,62 @@
+pub mod linearalg {
+    pub struct Tensor<T> {
+        pub shape: Vec<usize>,
+        pub data: Vec<T>
+    }
+
+    impl<T> Tensor<T> {
+        pub fn get_rank(&self) -> usize {
+            self.shape.len()
+        }
+        fn flatten_indices(&self, indices: &Vec<usize>) -> usize {
+            let mut scaled_index = 0;
+            let mut scale = 1;
+            for j in 0..indices.len() {
+                let i = indices.len() - j - 1;
+                let index = indices[i];
+                scaled_index += scale * index;
+                scale *= self.shape[i];
+            }
+
+            scaled_index
+        }
+        pub fn set_element(&mut self, indices: &Vec<usize>, value: T) {
+            self.data[self.flatten_indices(&indices)] = value;
+        }
+
+        pub fn slice(&self, index: usize) -> &[T]{
+            let mut slice_size = 1;
+            for j in 0..self.shape.len()-1 {
+                let i = self.shape.len() - j - 1;
+                slice_size *= self.shape[i];
+            }
+            let slice_start = slice_size*index;
+            let slice_end = slice_start + slice_size;
+
+            &self.data[slice_start..slice_end]
+        }
+
+        pub fn get_patch(&self, corner: &Vec<usize>, size: &Vec<usize>) -> &[T] {
+            let corner_index = self.flatten_indices(corner);
+            
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    
+    mod linearalg {
+        use crate::linearalg::Tensor;
+
+        #[test]
+        fn test_set_element(){
+            let mut t  = Tensor { shape: vec![2,2], data: vec![0; 4] };
+            t.set_element(&vec![1,1], 2);
+            assert!(t.data[3] == 2);
+            t  = Tensor { shape: vec![4,2,6], data: vec![0; 4*2*6] };
+            t.set_element(&vec![3,0,5], 2);
+            assert!(t.data[3*2*6+5] == 2);
+        }
     }
 }
---
+++ packages/neural/src/lib.rs
@@ -0,0 +1,36 @@
+pub mod architectures {
+    pub mod cnn {
+        use math::linearalg::Tensor;
+
+        pub fn convolve_filters_over_image(image: &Tensor<f32>, filters: &Tensor<f32>, bias: &Tensor<f32>, step: i32) -> Tensor<f32> {
+            assert!(filters.shape.len() > 0);
+            let number_of_filters = filters.shape[0];
+            let filter_size = filters.shape[1];
+            let image_size = image.shape[0];
+            let out_size = ((image_size - filter_size) / step) + 1;
+
+            let output = Tensor { 
+                shape: vec![number_of_filters, out_size, out_size], 
+                data: vec![0f32; (number_of_filters*out_size*out_size).try_into().unwrap()]
+            };
+
+            for filter_index in 0..number_of_filters {
+                let image_y = 0;
+                let output_y = 0;
+                while image_y + filter_size <= image_size {
+                    let image_x = 0;
+                    let output_x = 0;
+                    while image_x + filter_size <= image_size {
+                        let sum = 0f32;
+                        let filter = filters.slice(filter_index);
+                        let image_patch= image.get_patch(image_y, image_x, filter_size);
+                        
+                        output.set_element(vec![filter_index, image_y, image_x], 0f32);
+                    }
+                }
+            }
+
+            output
+        }
+    }
+}
